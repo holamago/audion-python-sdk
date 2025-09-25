5c089b66ffdfadc8a7030c1297a27a16655ab6b3
chanbyeong <chanbyeong@holamago.com>
2025-09-25 02:22:06 +0000

    refactor: remove unnecessary comments from __init__.py

--- audion/__init__.py
+++ audion/__init__.py
@@ -8,6 +8,6 @@ from .client import AudionClient
 from .version import __version__
 
 __all__ = [
-    "AudionClient",           # 메인 클라이언트 클래스
-    "__version__",            # SDK 버전 정보
+    "AudionClient",           
+    "__version__",            
 ]
